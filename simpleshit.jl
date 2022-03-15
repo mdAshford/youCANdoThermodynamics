fff37ffed2a85ba62fc60c7d5891378acc9f6a74
mdAshford <42947897+mdAshford@users.noreply.github.com>
2022-03-15 00:50:26 -0500

    Add files via upload

---
+++ simpleshit.jl
@@ -0,0 +1,98 @@
+### A Pluto.jl notebook ###
+# v0.18.2
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ e506c710-a422-11ec-08e1-cd6c187d3881
+using CommonMark
+
+# ╔═╡ 8d5d5caa-1012-4e4e-af93-9373bf8eac71
+
+
+# ╔═╡ 362039ef-eee1-4c1a-b1f9-b95b803d816b
+cm"""
+# Simple shit not working
+"""
+
+# ╔═╡ 8b295661-4a95-451f-97ae-1c8100de8adc
+cm"""
+```math
+x = \frac{\sqrt{x^2-1}}{32}
+```
+"""
+
+# ╔═╡ 582ac6a4-9e64-4021-b322-157c9ba7013f
+cm"""
+```math
+\ce{H2SO4}
+```
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+CommonMark = "a80b9123-70ca-4bc0-993e-6e3bcb318db6"
+
+[compat]
+CommonMark = "~0.8.6"
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.7.2"
+manifest_format = "2.0"
+
+[[deps.CommonMark]]
+deps = ["Crayons", "JSON", "URIs"]
+git-tree-sha1 = "4cd7063c9bdebdbd55ede1af70f3c2f48fab4215"
+uuid = "a80b9123-70ca-4bc0-993e-6e3bcb318db6"
+version = "0.8.6"
+
+[[deps.Crayons]]
+git-tree-sha1 = "249fe38abf76d48563e2f4556bebd215aa317e15"
+uuid = "a8cc5b0e-0ffa-5ad4-8c14-923d3ee1735f"
+version = "4.1.1"
+
+[[deps.Dates]]
+deps = ["Printf"]
+uuid = "ade2ca70-3891-5945-98fb-dc099432e06a"
+
+[[deps.JSON]]
+deps = ["Dates", "Mmap", "Parsers", "Unicode"]
+git-tree-sha1 = "3c837543ddb02250ef42f4738347454f95079d4e"
+uuid = "682c06a0-de6a-54ab-a142-c8b1cf79cde6"
+version = "0.21.3"
+
+[[deps.Mmap]]
+uuid = "a63ad114-7e13-5084-954f-fe012c677804"
+
+[[deps.Parsers]]
+deps = ["Dates"]
+git-tree-sha1 = "85b5da0fa43588c75bb1ff986493443f821c70b7"
+uuid = "69de0a69-1ddd-5017-9359-2bf0b02dc9f0"
+version = "2.2.3"
+
+[[deps.Printf]]
+deps = ["Unicode"]
+uuid = "de0858da-6303-5e67-8744-51eddeeeb8d7"
+
+[[deps.URIs]]
+git-tree-sha1 = "97bbe755a53fe859669cd907f2d96aee8d2c1355"
+uuid = "5c2747f8-b7ea-4ff2-ba2e-563bfd36b1d4"
+version = "1.3.0"
+
+[[deps.Unicode]]
+uuid = "4ec0a83e-493e-50e2-b9ac-8f72acf5a8f5"
+"""
+
+# ╔═╡ Cell order:
+# ╠═e506c710-a422-11ec-08e1-cd6c187d3881
+# ╠═8d5d5caa-1012-4e4e-af93-9373bf8eac71
+# ╟─362039ef-eee1-4c1a-b1f9-b95b803d816b
+# ╟─8b295661-4a95-451f-97ae-1c8100de8adc
+# ╟─582ac6a4-9e64-4021-b322-157c9ba7013f
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
